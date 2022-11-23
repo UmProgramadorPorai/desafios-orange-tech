979cabea11dba458909ef31b31d4895568c23ef6
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-11-23 18:23:46 -0300

    desafio github wiki

---
+++ desafio03-github-wiki/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  background-color: #fafafa;
+  border: 1px solid #fafafa;
+  border-radius: 20px;
+
+  width: 80%;
+  padding: .65rem;
+
+  margin: 1.25rem;
+
+  &:hover {
+    background-color: #fafafa60;
+    cursor: pointer;
+  }
+`;
